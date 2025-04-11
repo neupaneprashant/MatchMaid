734fc1b8eb52f333b5863f6a0a11592679d3da3c
ProductiveSnail0589 <33462355+lazysnail0589@users.noreply.github.com>
2025-04-11 16:11:16 -0500

    Updates

--- App.jsx
+++ App.jsx
@@ -4,7 +4,21 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App()  {
-
+    return(
+        <div className="App">
+            <header className="MatchMaid-header">
+                <img src={viteLogo} className="logo" alt="Vite logo" />
+                <img src={reactLogo} className="logo react" alt="React logo" />
+                <h1>Vite + React</h1>
+            </header>
+            <div className="card">
+                <button onClick={() => alert('Welcome to MatchMaid!')}>Click on me</button>
+            </div>
+            <p className="read-the-docs">
+                Click on the Vite and React logos to learn more
+            </p>
+        </div>
+    );
 
 }
 
