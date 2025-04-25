d780bd177a636ffda065a21d5fb1453b9c180e75
synthwulf <rcanderson318@gmail.com>
2025-04-25 11:17:21 -0500

    added chat page into App.jsx; modified firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -19,3 +19,5 @@ const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
+export { app }; //added this to import the chatpage: del later
