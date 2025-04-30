6b0409a92a08dd5aa8bde5b60518031674d6a80b
Prasiddha Neupane <neupaneprasiddha2004@gmail.com>
2025-04-30 17:27:51 -0500

    Just updated all the files and fixed issues.

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './landing';
 import Signup from './Signup';
 import TinderCards from './Tinder_Cards';
 import ReviewSection from './ReviewSection';
 import MyReviews from './MyReviews';
 import Bookings from './Bookings';
-import ChatPage from './ChatPage'; 
-import ReviewSection from './ReviewSection';
+import ChatPage from './ChatPage';
 import MaidPage from './MaidPage';
-
 
 import './App.css';
 import './signup.css';
 import './landing.css';
 import './Tinder_Cards.css';
-import React from 'react';
 
 function App() {
   return (
@@ -25,11 +22,12 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/tinder" element={<TinderCards />} />
         <Route path="/home" element={<TinderCards />} /> 
-        <Route path="/review" element={<ReviewSection />} />
+        <Route path="/tinder-shuffle/review" element={<ReviewSection />} />        
         <Route path="/my-reviews" element={<MyReviews />} />
         <Route path="/bookings" element={<Bookings />} />
         <Route path="/chat" element={<ChatPage />} /> 
         <Route path="/maidpage" element={<MaidPage />} />
+        <Route path="/profile" element={<ProfilePage/>}/>
       </Routes>
     </Router>
   );
