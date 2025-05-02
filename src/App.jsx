66917c1cec8562cf844acd9c9d03fc3fc076c414
MLGPJKing <prestonparker37@gmail.com>
2025-05-01 21:40:42 -0500

    Added routing for new pages.

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,38 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import React from 'react';
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 import LandingPage from './landing';
 import Signup from './Signup';
 import TinderCards from './Tinder_Cards';
-import ChatPage from './ChatPage'; 
-import ReviewSection from './ReviewSection';
-import MaidPage from './MaidPage';
-
-
 import './App.css';
 import './signup.css';
 import './landing.css';
 import './Tinder_Cards.css';
-import './ChatPage.css'; 
+import ReviewSection from './ReviewSection'; 
+import MaidPortal from './MaidPortal';
+import ChatPage from './ChatPage'; // Assuming you have a ChatPage component for chat functionality
+import ProfilePage from './MaidPage';
+import MyReviews from './MyReviews'; // Assuming you have a MyReviews component for displaying user reviews
+import React from 'react';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/home" element={<TinderCards />} />
-        <Route path="/tinder-shuffle/review" element={<ReviewSection />} />
-        <Route path="/chat" element={<ChatPage />} /> 
-        <Route path="/maidpage" element={<MaidPage />} />
-      </Routes>
-    </Router>
+    <PayPalScriptProvider options={{ "client-id": "AaWfHrfQT8-xfjy-UFHZmtNG2XkN1_OPeIapYMMazZGq16GRLXamgDQJh3fMHcpah-QW4edYzxmnzwXM" }}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/home" element={<TinderCards />} />
+          <Route path="/tinder-shuffle/review" element={<ReviewSection />} />
+          <Route path="/maid-portal" element={<MaidPortal />} />
+          <Route path="/maid-chat" element={<ChatPage />} />
+          <Route path="/maid-schedule" element={<ProfilePage />} />
+          <Route path="/my-reviews" element={<MyReviews />} />
+
+
+
+        </Routes>
+      </Router>
+    </PayPalScriptProvider>
   );
 }
 
