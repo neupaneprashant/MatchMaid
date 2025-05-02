eb4107394a6b45d403ac76508551a9689948fcec
MLGPJKing <prestonparker37@gmail.com>
2025-05-01 21:38:04 -0500

    Cleanup.

--- src/firebase.js
+++ src/firebase.js
@@ -19,5 +19,3 @@ const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-
-export { app }; //added this to import the chatpage: del later
